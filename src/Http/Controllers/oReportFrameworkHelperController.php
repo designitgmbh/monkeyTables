c9340dd6ecb55a967e88b4cd61dcdc9ab38743f5
Philipp Pajak <espspinix@gmail.com>
2015-10-28 16:45:18 +0100

    added oreport print controller helper

---
+++ src/Http/Controllers/oReportFrameworkHelperController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Designitgmbh\MonkeyTables\Http\Controllers;
+
+class oReportFrameworkHelperController extends oDataFrameworkHelperController
+{
+	//framework config variables
+	protected $_subjectTranslateString 	= "L_OREPORT_EXPORT_SUBJECT";
+	protected $_mailView 				= "emails.oReportExport";
+	protected $_printViewRoute			= "oReport.printView";
+}
