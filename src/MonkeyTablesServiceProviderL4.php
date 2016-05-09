3fa1fef6da300684fc18d2d2ddc2bb902da2b952
Philipp Pajak <espspinix@gmail.com>
2016-05-09 14:22:38 +0200

    added laravel 4 service provider

---
+++ src/MonkeyTablesServiceProviderL4.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Designitgmbh\MonkeyTables;
+
+use Illuminate\Support\ServiceProvider;
+
+class MonkeyTablesServiceProviderL4 extends ServiceProvider
+{
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('designitgmbh/monkeyTables', 'monkeyTables');
+
+        require __DIR__.'/Http/routes.php';
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @codeCoverageIgnore
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('monkeyTables',function($app){
+            return new MonkeyTables($app);
+        });
+    }
+
+}
